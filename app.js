7b9802409ed9f005c6f24cff6f11a0af5d715000
masudrana <masudranaacb@gmail.com>
2022-03-03 09:46:26 +0600

    set time out using

--- app.js
+++ app.js
@@ -3,10 +3,16 @@
 
 // set time out usning
 
-const student=()=>{
-    console.log('i am a student')
+const student1=()=>{
+    console.log('i am a student1')
+}
+const student2=()=>{
+    console.log('i am a student2')
 }
 console.log('i need help')
 console.log('i am happy')
-student()
+
+setTimeout(student1,5000)
+setTimeout(student2,4000)
+
 console.log('i am so sad')
