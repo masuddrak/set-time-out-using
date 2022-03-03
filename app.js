03836c49ee1875ee0da7340508b166597e00a45e
masudrana <masudranaacb@gmail.com>
2022-03-03 09:43:33 +0600

    normal script

---
+++ app.js
@@ -0,0 +1,12 @@
+
+
+
+// set time out usning
+
+const student=()=>{
+    console.log('i am a student')
+}
+console.log('i need help')
+console.log('i am happy')
+student()
+console.log('i am so sad')
